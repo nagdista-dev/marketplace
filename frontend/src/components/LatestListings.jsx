4c53061427a4c39cca0f9e764ff37371cec24185
nagdista <nagdista@gmail.com>
2025-12-08 12:52:19 +0200

    completed title component

---
+++ frontend/src/components/LatestListings.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Title from "./Title";
+
+const LatestListings = () => {
+  return (
+    <div className="mt-20">
+      <Title
+        title="Latest Listings"
+        description="Discover the hottest social profiles available right now."
+      />
+    </div>
+  );
+};
+
+export default LatestListings;
